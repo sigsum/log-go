98bf8bf2c78158f60dc8ea229cdb8c8e94b2e2c3
Niels Möller <nisse@glasklarteknik.se>
2022-09-12 09:27:02 +0200

    Add copyright headers for files copied from trillian

--- integration/resetdb.sh
+++ integration/resetdb.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Copyright 2019 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Based on
 # https://github.com/google/trillian/blob/master/scripts/resetdb.sh,
-# as of commit e441824 on May 14, 2019.
+# as of commit e441824 on May 14, 2019. Note original file lacked
+# copyright headers, added above based on top-level trillian LICENCE
+# and git history.
+
 set -e
 
 usage() {
--- integration/storage.sql
+++ integration/storage.sql
@@ -1,8 +1,24 @@
 # MySQL / MariaDB version of the tree schema
 
+-- Copyright 2020 Google LLC. All Rights Reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- Database schema from
 -- https://github.com/google/trillian/blob/master/storage/mysql/schema/storage.sql,
--- as of commit e1ac8ba on Feb 18, 2021
+-- as of commit e1ac8ba on Feb 18, 2021. Note original file lacked
+-- copyright headers, added above based on top-level trillian LICENCE
+-- and git history.
 
 -- ---------------------------------------------
 -- Tree stuff here
