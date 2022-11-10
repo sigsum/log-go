c4c92bdc1b1ed172a719d5b15ca755b3494103f0
Niels Möller <nisse@glasklarteknik.se>
2022-11-10 11:09:34 +0100

    Add test for public suffixes

--- internal/rate-limit/limiter_test.go
+++ internal/rate-limit/limiter_test.go
@@ -24,7 +24,7 @@ func (c *fakeClock) Now() time.Time {
 func (c *fakeClock) Advance(delta time.Duration) {
 	c.Lock()
 	defer c.Unlock()
-	c.now.Add(delta)
+	c.now = c.now.Add(delta)
 }
 
 func newTestLimiter(config string, clock clocker) (Limiter, error) {
@@ -55,7 +55,7 @@ func repeatedAccess(t *testing.T, config string, count int, requests []request) 
 	return count
 }
 
-func testNoConfig(t *testing.T) {
+func TestNoConfig(t *testing.T) {
 	keyHash := crypto.Hash{1}
 
 	if repeatedAccess(t, "", 1, []request{request{domain: nil, keyHash: &keyHash, delay: time.Hour}}) != 0 {
@@ -68,7 +68,7 @@ func testNoConfig(t *testing.T) {
 	}
 }
 
-func testKeyLimit(t *testing.T) {
+func TestKeyLimit(t *testing.T) {
 	key1 := crypto.Hash{1}
 	key2 := crypto.Hash{2}
 	config := fmt.Sprintf("key %x 25 \nkey %x 23\n", key1, key2)
@@ -89,11 +89,11 @@ func testKeyLimit(t *testing.T) {
 	}
 }
 
-func testDomainLimit(t *testing.T) {
+func TestDomainLimit(t *testing.T) {
 	A := func(s string) *string { return &s }
 	key := crypto.Hash{}
 	config := "domain foo.example.com 25\n" +
-		"domain foo.example.org 23" +
+		"domain foo.example.org 23\n" +
 		"domain www.foo.example.org 13\n"
 
 	if got := repeatedAccess(t, config, 100,
@@ -113,10 +113,10 @@ func testDomainLimit(t *testing.T) {
 	}
 	if got := repeatedAccess(t, config, 100,
 		[]request{
-			request{domain: A("foo.Example.com"), keyHash: &key, delay: time.Hour},
-			request{domain: A("www.foo.Example.COM"), keyHash: &key, delay: time.Hour},
-		}); got != 25 {
-		t.Errorf("limit of 25 request applies also to subdomains, but failed after %d requests", got)
+			request{domain: A("foo.Example.org"), keyHash: &key, delay: time.Hour},
+			request{domain: A("under.foo.Example.org"), keyHash: &key, delay: time.Hour},
+		}); got != 23 {
+		t.Errorf("limit of 23 request applies also to subdomains, but failed after %d requests", got)
 	}
 	if got := repeatedAccess(t, config, 100,
 		[]request{
@@ -125,5 +125,41 @@ func testDomainLimit(t *testing.T) {
 		}); got != 100 {
 		t.Errorf("should sustain one request per hour, when subdomain has its own configured limit, but failed after %d requests", got)
 
+	}
+}
+
+func TestPublicLimit(t *testing.T) {
+	A := func(s string) *string { return &s }
+	key := crypto.Hash{}
+	// Test config with only net and org
+	config := "public test_suffix_list.dat 23\n"
+	if got := repeatedAccess(t, config, 100,
+		[]request{request{domain: A("foo.Example.com"), keyHash: &key, delay: time.Hour}}); got != 0 {
+		t.Errorf("unknown suffixes should be denied, but %d requests were allowed", got)
+	}
+	if got := repeatedAccess(t, config, 100,
+		[]request{request{domain: A("foo.Example.org"), keyHash: &key, delay: time.Hour}}); got != 23 {
+		t.Errorf("limit of 23 request per 24 hours not enforced, %d requests were allowed", got)
+	}
+	if got := repeatedAccess(t, config, 100,
+		[]request{
+			request{domain: A("foo.Example.org"), keyHash: &key, delay: time.Hour},
+			request{domain: A("bar.Example.ORG"), keyHash: &key, delay: time.Hour},
+		}); got != 23 {
+		t.Errorf("limit of 23 request (on example.org domains) per 24 hours not enforced, %d requests were allowed", got)
+	}
+	if got := repeatedAccess(t, config, 100,
+		[]request{
+			request{domain: A("foo.Example.org"), keyHash: &key, delay: time.Hour},
+			request{domain: A("bar.other.org"), keyHash: &key, delay: time.Hour},
+		}); got != 100 {
+		t.Errorf("should sustain one request per hour, when alternating registered domain, but failed after %d requests", got)
+	}
+	if got := repeatedAccess(t, config, 100,
+		[]request{
+			request{domain: A("foo.Example.org"), keyHash: &key, delay: time.Hour},
+			request{domain: A("bar.other.org"), keyHash: &key, delay: time.Hour},
+		}); got != 100 {
+		t.Errorf("should sustain one request per hour, when alternating public suffix, but failed after %d requests", got)
 	}
 }
