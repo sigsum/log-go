4b5fa669c8f7c3159d00c4b77006f10416ab7fd4
Niels Möller <nisse@glasklarteknik.se>
2023-05-08 11:34:20 +0200

    Use internal/mocks/db instead, add a Ptr matcher

--- internal/witness/witness_test.go
+++ internal/witness/witness_test.go
@@ -2,9 +2,13 @@ package witness
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+
+	"sigsum.org/log-go/internal/mocks/db"
 
 	"sigsum.org/sigsum-go/pkg/client"
 	"sigsum.org/sigsum-go/pkg/crypto"
@@ -13,26 +17,36 @@ import (
 	"sigsum.org/sigsum-go/pkg/types"
 )
 
-func testWitness(ctrl *gomock.Controller) (*mocks.MockLogClient, *mocks.MockWitnessClient, *witness) {
+func testWitness(ctrl *gomock.Controller, f GetConsistencyProofFunc) (*mocks.MockWitnessClient, *witness) {
 	client := mocks.NewMockWitnessClient(ctrl)
-	// Mocked only for the GetConsistencyProof method.
-	log := mocks.NewMockLogClient(ctrl)
-	return log, client, &witness{
-		client: client,
-		// Some pointer/value impedance mismatch.
-		getConsistencyProof: func(ctx context.Context, req *requests.ConsistencyProof) (types.ConsistencyProof, error) {
-			return log.GetConsistencyProof(ctx, *req)
-		},
+	return client, &witness{
+		client:              client,
+		getConsistencyProof: f,
 	}
 }
 
+type ptrMatcher struct {
+	m gomock.Matcher
+}
+
+func (p ptrMatcher) Matches(x any) bool {
+	return x != nil && p.m.Matches(reflect.ValueOf(x).Elem().Interface())
+}
+func (p ptrMatcher) String() string {
+	return fmt.Sprintf("non-nil pointer to %s", p.m)
+}
+func Ptr(m gomock.Matcher) gomock.Matcher {
+	return ptrMatcher{m: m}
+}
 func TestWitnessEmpty(t *testing.T) {
 	testTimestamp := uint64(101010)
 	ctrl := gomock.NewController(t)
-	log, cli, w := testWitness(ctrl)
+	log := db.NewMockClient(ctrl)
+	cli, w := testWitness(ctrl, log.GetConsistencyProof)
+
 	sth := types.SignedTreeHead{TreeHead: types.TreeHead{Size: 5}}
 
-	log.EXPECT().GetConsistencyProof(gomock.Any(), gomock.Eq(requests.ConsistencyProof{OldSize: 0, NewSize: 5})).Return(types.ConsistencyProof{}, nil)
+	log.EXPECT().GetConsistencyProof(gomock.Any(), Ptr(gomock.Eq(requests.ConsistencyProof{OldSize: 0, NewSize: 5}))).Return(types.ConsistencyProof{}, nil)
 	cli.EXPECT().AddTreeHead(gomock.Any(), gomock.Any()).DoAndReturn(
 		func(_ context.Context, req requests.AddTreeHead) (types.Cosignature, error) {
 			if req.OldSize != 0 || req.TreeHead != sth {
@@ -53,11 +67,13 @@ func TestWitnessEmpty(t *testing.T) {
 func TestWitnessBadSize(t *testing.T) {
 	testTimestamp := uint64(101010)
 	ctrl := gomock.NewController(t)
-	log, cli, w := testWitness(ctrl)
+	log := db.NewMockClient(ctrl)
+	cli, w := testWitness(ctrl, log.GetConsistencyProof)
+
 	sth := types.SignedTreeHead{TreeHead: types.TreeHead{Size: 5}}
 
-	log.EXPECT().GetConsistencyProof(gomock.Any(), gomock.Eq(requests.ConsistencyProof{OldSize: 0, NewSize: 5})).Return(types.ConsistencyProof{}, nil)
-	log.EXPECT().GetConsistencyProof(gomock.Any(), gomock.Eq(requests.ConsistencyProof{OldSize: 2, NewSize: 5})).Return(
+	log.EXPECT().GetConsistencyProof(gomock.Any(), Ptr(gomock.Eq(requests.ConsistencyProof{OldSize: 0, NewSize: 5}))).Return(types.ConsistencyProof{}, nil)
+	log.EXPECT().GetConsistencyProof(gomock.Any(), Ptr(gomock.Eq(requests.ConsistencyProof{OldSize: 2, NewSize: 5}))).Return(
 		// Dummy path, but length 1 to distinguish the two calls.
 		types.ConsistencyProof{Path: []crypto.Hash{crypto.Hash{}}}, nil)
 	cli.EXPECT().AddTreeHead(gomock.Any(), gomock.Any()).DoAndReturn(
