526ee4aa6e8dde64e8b01544c8071409125de939
Niels Möller <nisse@glasklarteknik.se>
2023-05-08 16:15:53 +0200

    Add test of GetCosignatures

--- internal/witness/witness_test.go
+++ internal/witness/witness_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 
@@ -109,6 +110,67 @@ func TestWitnessBadSize(t *testing.T) {
 	}
 	if cs.Timestamp != testTimestamp {
 		t.Errorf("unexpected timestamp, got %d, want: %d", cs.Timestamp, testTimestamp)
+	}
+}
+
+func TestGetCosignatures(t *testing.T) {
+	testTimestamp := uint64(101010)
+	logPub, logSigner := mustKeyPair(t)
+	logKeyHash := crypto.HashBytes(logPub[:])
+
+	ctrl := gomock.NewController(t)
+	log := db.NewMockClient(ctrl)
+
+	signer1, cli1, w1 := testWitness(t, ctrl, &logKeyHash, log.GetConsistencyProof)
+	_, cli2, w2 := testWitness(t, ctrl, &logKeyHash, log.GetConsistencyProof)
+	signer3, cli3, w3 := testWitness(t, ctrl, &logKeyHash, log.GetConsistencyProof)
+
+	sth := mustSignTreehead(t, logSigner, 5)
+
+	log.EXPECT().GetConsistencyProof(gomock.Any(), Ptr(gomock.Eq(requests.ConsistencyProof{OldSize: 0, NewSize: 5}))).Return(types.ConsistencyProof{}, nil).AnyTimes()
+	log.EXPECT().GetConsistencyProof(gomock.Any(), Ptr(gomock.Eq(requests.ConsistencyProof{OldSize: 2, NewSize: 5}))).Return(
+		// Dummy path, but length 1 to distinguish the two calls.
+		types.ConsistencyProof{Path: []crypto.Hash{crypto.Hash{}}}, nil)
+
+	// First witness needs size query.
+	cli1.EXPECT().AddTreeHead(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(_ context.Context, req requests.AddTreeHead) (types.Cosignature, error) {
+			if req.OldSize != 0 || req.TreeHead != sth || len(req.Proof.Path) != 0 {
+				t.Fatalf("unexpected add tree head req, got: %v", req)
+			}
+			return types.Cosignature{}, client.HttpUnprocessableEntity
+		})
+	cli1.EXPECT().GetTreeSize(gomock.Any(), gomock.Any()).Return(uint64(2), nil)
+	cli1.EXPECT().AddTreeHead(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(_ context.Context, req requests.AddTreeHead) (types.Cosignature, error) {
+			if req.OldSize != 2 || req.TreeHead != sth || len(req.Proof.Path) != 1 {
+				t.Fatalf("unexpected add tree head req, got: %v", req)
+			}
+			return mustCosign(t, signer1, &req.TreeHead.TreeHead, &logKeyHash, testTimestamp), nil
+		})
+	// Second witness fails.
+	cli2.EXPECT().AddTreeHead(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(_ context.Context, req requests.AddTreeHead) (types.Cosignature, error) {
+			if req.OldSize != 0 || req.TreeHead != sth || len(req.Proof.Path) != 0 {
+				t.Fatalf("unexpected add tree head req, got: %v", req)
+			}
+			return types.Cosignature{}, fmt.Errorf("mock failure")
+		})
+
+	// Third witness succeeds with slight delay.
+	cli3.EXPECT().AddTreeHead(gomock.Any(), gomock.Any()).DoAndReturn(
+		func(_ context.Context, req requests.AddTreeHead) (types.Cosignature, error) {
+			if req.OldSize != 0 || req.TreeHead != sth || len(req.Proof.Path) != 0 {
+				t.Fatalf("unexpected add tree head req, got: %v", req)
+			}
+			time.Sleep(50 * time.Millisecond)
+			return mustCosign(t, signer3, &req.TreeHead.TreeHead, &logKeyHash, testTimestamp), nil
+		})
+	collector := CosignatureCollector{witnesses: []*witness{w1, w2, w3}}
+
+	cosignatures := collector.GetCosignatures(context.Background(), &sth)
+	if got, want := len(cosignatures), 2; got != want {
+		t.Errorf("unexpected number of cosignatures, got: %d, want: %d", got, want)
 	}
 }
 
