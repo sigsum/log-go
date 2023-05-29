20ab1b0dcde4872b3960f7e2de76b5f5afaa6461
Niels Möller <nisse@glasklarteknik.se>
2023-05-29 10:41:16 +0200

    Don't depend on token.Verifier interface
    Define our own interface and corresponding mock type for tests.

--- internal/node/primary/primary.go
+++ internal/node/primary/primary.go
@@ -17,7 +17,7 @@ type Primary struct {
 	MaxRange      int                // Maximum number of leaves per get-leaves request
 	DbClient      db.Client          // provides access to the backend, usually Trillian
 	Stateman      state.StateManager // coordinates access to (co)signed tree heads
-	TokenVerifier token.Verifier     // checks if domain name knows a public key
+	TokenVerifier *token.DnsVerifier // checks if domain name knows a public key
 	RateLimiter   rateLimit.Limiter
 }
 
